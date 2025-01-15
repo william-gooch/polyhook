856ef78cb1641c5603a0c3c81a4123b5d02c647f
William <william.gooch.wg@gmail.com>
2025-01-15 17:59:33 +0000

    Add new examples

--- hooklib/src/examples.rs
+++ hooklib/src/examples.rs
@@ -5,8 +5,12 @@ pub const EXAMPLES: &[(&str, &str)] = &[
     ("Sphere", EXAMPLE_SPHERE),
     ("Dynamic Circle", EXAMPLE_DYNAMIC_CIRCLE),
     ("Sew", EXAMPLE_SEW),
+    ("Moon", EXAMPLE_MOON),
+    ("Star", EXAMPLE_STAR),
     ("Bear", EXAMPLE_BEAR),
     ("Snowman", EXAMPLE_SNOWMAN),
+    ("Snorlax", EXAMPLE_SNORLAX),
+    ("V-Stitch", EXAMPLE_VSTITCH),
 ];
 
 pub const EXAMPLE_FLAT: &str           = concat!(file!(), "/../../examples/flat.ph");
@@ -14,6 +18,10 @@ pub const EXAMPLE_SPIRAL_ROUNDS: &str  = concat!(file!(), "/../../examples/spira
 pub const EXAMPLE_JOINED_ROUNDS: &str  = concat!(file!(), "/../../examples/joined_rounds.ph");
 pub const EXAMPLE_SPHERE: &str         = concat!(file!(), "/../../examples/sphere.ph");
 pub const EXAMPLE_DYNAMIC_CIRCLE: &str = concat!(file!(), "/../../examples/dynamic_circle.ph");
+pub const EXAMPLE_MOON: &str           = concat!(file!(), "/../../examples/moon.ph");
+pub const EXAMPLE_STAR: &str           = concat!(file!(), "/../../examples/star.ph");
 pub const EXAMPLE_SEW: &str            = concat!(file!(), "/../../examples/sew.ph");
 pub const EXAMPLE_BEAR: &str           = concat!(file!(), "/../../examples/bear.ph");
 pub const EXAMPLE_SNOWMAN: &str        = concat!(file!(), "/../../examples/snowman.ph");
+pub const EXAMPLE_SNORLAX: &str        = concat!(file!(), "/../../examples/snorlax.ph");
+pub const EXAMPLE_VSTITCH: &str        = concat!(file!(), "/../../examples/vstitch.ph");
